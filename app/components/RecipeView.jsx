204606509e8d8f5b2e00283b351d4229ae8a9e14
Thomas Johnson <thomasij813@gmail.com>
2016-03-15 17:34:40 -0700

    Get basic scaffolding of recipe view

--- app/components/RecipeView.jsx
+++ app/components/RecipeView.jsx
@@ -1,25 +1,55 @@
 import React from 'react'
 
 let style = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  height: '125px',
-  marginLeft: '10px',
-  width: '100%'
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: '0 25px',
+    width: '100%'
+  },
+  recipe: {
+    flexGrow: 1
+  },
+  button: {
+    marginTop: '10px'
+  },
+  preWrap: {
+    whiteSpace: 'pre-wrap'
+  }
 }
 
 function NoRecipeMessage(props) {
   return <h1>Click New Recipe to add a recipe.</h1>
 }
 
+function IngredientList(props) {
+  return (
+    <ul>
+      {props.ingredients.map((ingredient, index) => {
+        return <li key={index}>{ingredient}</li>
+      })}
+    </ul>
+  )
+}
+
+function Recipe(props) {
+  return (
+    <div style={style.recipe}>
+      <h1>{props.recipe.title}</h1>
+      <IngredientList ingredients={props.recipe.ingredients} />
+      <div style={style.preWrap}>{props.recipe.text}</div>
+    </div>
+  )
+}
+
 function RecipeView(props) {
   if (props.recipe) {
     return (
-      <div style={style}>
-        <p>{props.recipe.text}</p>
-        <button onClick={props.handleEditClick}>Edit</button>
-        <button onClick={props.handleDeleteClick}>Delete</button>
+      <div style={style.wrapper}>
+        <Recipe recipe={props.recipe} />
+        <button onClick={props.handleEditClick} style={style.button}>Edit</button>
+        <button onClick={props.handleDeleteClick} style={style.button}>Delete</button>
       </div>
     )
   } else {
