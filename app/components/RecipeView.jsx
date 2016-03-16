f2a6fa3544d41550ec1cbd5bdb1a5538fed234b7
Thomas Johnson <thomasij813@gmail.com>
2016-03-15 17:05:58 -0700

    Refactor RecipeView component

--- app/components/RecipeView.jsx
+++ app/components/RecipeView.jsx
@@ -5,32 +5,42 @@ let style = {
   flexDirection: 'column',
   justifyContent: 'space-between',
   height: '125px',
-  marginLeft: '10px'
+  marginLeft: '10px',
+  width: '100%'
 }
 
 function NoRecipeMessage(props) {
   return <h1>Click New Recipe to add a recipe.</h1>
 }
 
-class RecipeView extends React.Component {
-  handleDelete(e) {
-    e.preventDefault()
-    this.props.deleteRecipe(this.props.recipeIndex)
-  }
-
-  render() {
-    if (this.props.recipe) {
-      return (
-        <div style={style}>
-          <p>{this.props.recipe.text}</p>
-          <button onClick={this.props.setView.bind(null, 'edit')}>Edit</button>
-          <button onClick={this.handleDelete.bind(this)}>Delete</button>
-        </div>
-      )
-    } else {
-      return <NoRecipeMessage />
-    }
+function RecipeView(props) {
+  if (props.recipe) {
+    return (
+      <div style={style}>
+        <p>{props.recipe.text}</p>
+        <button onClick={props.handleEditClick}>Edit</button>
+        <button onClick={props.handleDeleteClick}>Delete</button>
+      </div>
+    )
+  } else {
+    return <NoRecipeMessage />
   }
 }
+
+// class RecipeView extends React.Component {
+//   render() {
+//     if (this.props.recipe) {
+//       return (
+//         <div style={style}>
+//           <p>{this.props.recipe.text}</p>
+//           <button onClick={this.props.handleEditClick}>Edit</button>
+//           <button onClick={this.props.handleDeleteClick}>Delete</button>
+//         </div>
+//       )
+//     } else {
+//       return <NoRecipeMessage />
+//     }
+//   }
+// }
 
 export default RecipeView
--- app/containers/RecipeViewContainer.jsx
+++ app/containers/RecipeViewContainer.jsx
@@ -4,13 +4,22 @@ import RecipeView from '../components/RecipeView'
 import RecipeCreationFormContainer from './RecipeCreationFormContainer'
 
 class RecipeViewContainer extends React.Component {
+  handleDelete(e) {
+    e.preventDefault()
+    this.props.deleteRecipe(this.props.recipeIndex)
+  }
+
+  handleEdit(e) {
+    e.preventDefault()
+    this.props.setView('edit')
+  }
+
   render() {
     if (this.props.view === 'recipe') {
       return <RecipeView
         recipe={this.props.recipe}
-        recipeIndex={this.props.recipeIndex}
-        setView={this.props.setView}
-        deleteRecipe={this.props.deleteRecipe}/>
+        handleEditClick={this.handleEdit.bind(this)}
+        handleDeleteClick={this.handleDelete.bind(this)}/>
     }
 
     if (this.props.view === 'edit') {
